97d8eee4e92a7b8337fbf9acc44d36a21ffaa3a9
Pappa <dr.pappa@gmail.com>
2024-06-25 11:31:00 +0100

    Recommender pipeline

--- notebooks/pipelines/code/user_item_similarity_matrix_transformer.py
+++ notebooks/pipelines/code/user_item_similarity_matrix_transformer.py
@@ -14,9 +14,8 @@ if __name__ == "__main__":
     base_dir = "/opt/ml/processing"
     data_path = "user_item_ratings.csv"
 
-    pd.read_csv(f"{base_dir}/{data_path}", dtype=data_types)
+    user_item_ratings = pd.read_csv(f"{base_dir}/{data_path}", dtype=data_types)
 
-    
     transformer = SKPipeline(
         [
             ("user_item", UserItemMatrixTransformer()),
@@ -24,4 +23,6 @@ if __name__ == "__main__":
         ]
     )
 
-    return transformer
+    similarity_matrix = pipe.transform(user_item_ratings)
+
+    similarity_matrix.to_csv(f"{base_dir}/user_item_similarity_matrix.csv", header=True, index=False)
