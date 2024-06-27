0e32e852519bfff52fe7c87e8ae87a3481212479
Pappa <dr.pappa@gmail.com>
2024-06-27 15:37:36 +0100

    ItemRecommender

--- src/pipeliner/recommendations/recommender.py
+++ src/pipeliner/recommendations/recommender.py
@@ -2,223 +2,79 @@ import numpy as np
 import pandas as pd
 from sklearn.base import (
     BaseEstimator,
-    ClassifierMixin,
 )
-from sklearn.multioutput import MultiOutputClassifier
+from collections.abc import Sequence
 
 
-# class BaseRecommender(BaseEstimator, ClassifierMixin):
-#     """Defines the basic structure for an Recommender.
-
-#     TODO: describe how the recommender interacts with data
-#     """
-
-#     def __init__(self):
-#         pass
-
-#     def fit(self, X, y, sample_weight=None):
-#         return self
-
-#     def predict(self, X):
-#         pass
-
-#     def predict_proba(self, X):
-#         pass
-
-#     def predict_log_proba(self, X):
-#         pass
-
-#     def score(self, X, y, sample_weight=None):
-#         pass
-
-
-class ItemBasedRecommender(BaseEstimator):
+class ItemRecommender(BaseEstimator):
     """Item-based collaborative filtering recommender."""
 
-    user_item_matrix: pd.DataFrame
+    n: int
     similarity_matrix: pd.DataFrame
+    user_item_matrix: pd.DataFrame
+    item_similarity_threshold: float
 
-    def __init__(self, n=None):
+    def __init__(self, n=5, item_similarity_threshold=0.1):
         self.n = n
+        self.item_similarity_threshold = item_similarity_threshold
 
-    def fit(self, X, y=None, sample_weight=None):
+    def fit(self, X: pd.DataFrame | Sequence[pd.DataFrame], y=None):
         """Fits the recommender to the given data.
 
         Args:
-          X (DataFrame): Single DataFrame with similarity matrix or tuple/array of form (user-item matrix, similarity matrix)
+          X (pd.DataFrame | tuple[pd.DataFrame, pd.DataFrame]):
+            Single DataFrame with similarity matrix
+            or Sequence of (similarity matrix, user/item matrix)
         """
-        # store the similarity matrix internally
-        if isinstance(X, list) or isinstance(X, tuple):
-            self.sim_mat = X[1]
-            self.user_item = X[0]
-
-            # generate the sort matrix
-            if self.sort is not None:
-                self.sort_mat = pd.DataFrame(
-                    self.user_item.aggregate(self.sort, axis=1), columns=["ranking"]
-                )
+        if isinstance(X, pd.DataFrame):
+            self.similarity_matrix = X
+        elif isinstance(X, Sequence) and not isinstance(X, str):
+            self.similarity_matrix = X[0]
+            self.user_item_matrix = X[1]
         else:
-            self.sim_mat = X
+            raise ValueError("Input should be a DataFrame or a sequence of DataFrames")
 
         return self
 
-    def _retrieve_preds(self, item):
-        """Retrieve the similar items."""
-        pred = self.sim_mat.loc[item]
+    def _recommend(self, item: str | Sequence[str]):
+        if isinstance(item, str):
+            item_recommendations = (
+                self.similarity_matrix[
+                    self.similarity_matrix[item] > self.item_similarity_threshold
+                ][item]
+                .drop(item, errors="ignore")
+                .sort_values(ascending=False)
+            )
+            return np.array(item_recommendations.head(self.n).index)
+        elif isinstance(item, Sequence):
+            user_id, item_id = item[0], item[1]
 
-        # check for sort matrix
-        if self.sort_mat is None:
-            pred = pred.sort_values(ascending=self.ascending)
-        else:
-            pred = pd.DataFrame(pred)
-            pred.columns = ["similarity"]
-            pred = pred.join(self.sort_mat).sort_values(
-                by=["similarity", "ranking"], ascending=self.ascending
+            single_user_matrix = self.user_item_matrix.loc[user_id]
+            user_rated_items = single_user_matrix[single_user_matrix > 0].sort_values(
+                ascending=False
             )
 
-        # drop initial item and return
-        pred = pred.drop(labels=[item])
-        return pred
-
-    def predict(self, X):
-        """Predicts the desired number of outputs.
-
-        Returns:
-          Array of shape (X.shape[0], num_items)
-        """
-        res = []
-        for item in X:
-            try:
-                pred = self._retrieve_preds(item)
-                res.append(pred.index[: self.num_items])
-            except:
-                res.append(np.full(self.num_items, np.nan))
-        return np.array(res)
-
-    def predict_proba(self, X):
-        """Predicts the desired number of outputs and returns normalized scores with them."""
-        res = []
-        for item in X:
-            try:
-                pred = self._retrieve_preds(item)
-                res.append(
-                    list(
-                        zip(pred.index[: self.num_items], pred.values[: self.num_items])
-                    )
-                )
-            except:
-                res.append(np.full(self.num_items, (np.nan, np.nan)))
-        return np.array(res)
-
-
-class CrossSimilarityRecommender(MultiOutputClassifier):
-    """Recommender that uses similarities along one dimension and uses them to recommend items along a different dimension.
-
-    The goal of this recommender is to recommend items through the similarity of users (collaborative-filtering)
-
-    ranking matrix should be a series with items as index
-
-    Args:
-      num_items (int): The number of items to recommend
-      similarity (Recommender): Recommender that is used to generate the similarity scores between different users (should be a function with no input values)
-      filter (func): Function that retrieves a pandas series from the user-item matrix and returns a true/false series, which items the user interacted with
-      selection (str): Defines the type of item selection if the possible items exceed the result (options: array, sample, ranking)
-      rank_mat (DataFrame): Matrix that contains the ranking for individual items to select for `ranking` option (can be passed by fit function Alternatively)
-    """
-
-    def __init__(
-        self,
-        num_items=5,
-        similarity=SimilarityRecommender,
-        filter=None,
-        selection="array",
-        rank_mat=None,
-    ):
-        self.num_items = num_items
-        self.estimator = similarity()
-
-        # define sort function
-        if filter is None:
-            self.filter = lambda x: x > 0
+            item_recommendations = (
+                self.similarity_matrix[
+                    self.similarity_matrix[item_id] > self.item_similarity_threshold
+                ][item_id]
+                .drop([item_id] + user_rated_items.index, errors="ignore")
+                .sort_values(ascending=False)
+            )
+            return np.array(item_recommendations.head(self.n).index)
         else:
-            self.filter = filter
+            return np.array([])
 
-        self.selection = selection
-        self.rank_mat = rank_mat
-
-    def fit(self, X, y=None, sample_weight=None):
-        """Stores structure of the dataset in the recommender.
+    def predict(self, X: Sequence[str] | Sequence[Sequence[str]]):
+        """Predicts n recommendations
 
         Args:
-          X (tuple): Tuple/Array of data items in form (user-item-matrix, user-user-similarity-matrix).
+          X (Sequence): List of item_ids or list of (user_id, item_id) pairs
+
+        Returns:
+          np.array of shape (X.shape[0], n)
         """
-        # fit the internal estimator
-        self.estimator.fit([X[0], X[1]], y, sample_weight)
-        self.user_item = X[0]
-        # check for ranking matrix
-        if not isinstance(X, tuple) and len(X) > 2:
-            self.rank_mat = X[2]
-
-        return self
-
-    def predict(self, X):
-        """Predicts list of `num_items` for the given user_ids."""
-        # safty checks
-        if self.selection == "ranking" and self.rank_mat is None:
-            raise RuntimeError(
-                "No Ranking Matrix provided, but ranking option choosen!"
-            )
-
-        # prepare data
-        res = []
-
-        # retrieve similar users for each input users
-        sim_users = self.estimator.predict(X)
-        for x, susers in zip(X, sim_users):
-            # retrieve items the user already interacted with
-            try:
-                seen = self.user_item.loc[x]
-                seen = np.array(seen[self.filter(seen)].index)
-            except:
-                seen = []
-
-            # iterate through all similar users and find relevant items
-            user_rec = []
-            for user in susers:
-                try:
-                    cur_rec = self.user_item.loc[user]
-                    cur_rec = np.array(cur_rec[self.filter(cur_rec)].index)
-                except:
-                    continue
-
-                # remove items already seen by current users
-                cur_rec = np.setdiff1d(cur_rec, seen)
-
-                # combine items check exit condition
-                if len(user_rec) + len(cur_rec) >= self.num_items:
-                    len_rem = self.num_items - len(user_rec)
-                    # select relevant items based on option
-                    if self.selection == "sample":
-                        cur_rec = np.random.choice(cur_rec, size=len_rem, replace=False)
-                    elif self.selection == "ranking":
-                        # join items based on their value
-                        cur_rec = np.array(
-                            self.rank_mat.loc[cur_rec]
-                            .sort_values(ascending=False)
-                            .index
-                        )
-                        cur_rec = cur_rec[:len_rem]
-                    else:
-                        cur_rec = cur_rec[:len_rem]
-
-                    # append data to final list
-                    user_rec = np.append(user_rec, cur_rec)
-                    break
-                else:
-                    user_rec = np.append(user_rec, cur_rec)
-
-            res.append(np.array(user_rec).astype(self.user_item.columns.dtype))
-        return np.array(res)
+        return np.array([self._recommend(item) for item in X])
 
     def predict_proba(self, X):
-        pass
+        raise NotImplementedError("predict_proba not implemented yet")
