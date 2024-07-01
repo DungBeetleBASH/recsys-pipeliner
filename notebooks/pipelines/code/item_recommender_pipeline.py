fdaa368765c3c9f911cc98bb299a0ad1237fc1e6
Pappa <dr.pappa@gmail.com>
2024-07-01 20:19:53 +0100

    RecommenderPipeline

--- notebooks/pipelines/code/item_recommender_pipeline.py
+++ notebooks/pipelines/code/item_recommender_pipeline.py
@@ -3,7 +3,8 @@ from sagemaker import ScriptProcessor
 from sagemaker.workflow.pipeline_context import LocalPipelineSession
 from sagemaker.workflow.parameters import ParameterString
 from sagemaker.workflow.pipeline import Pipeline
-from sagemaker.workflow.steps import ProcessingStep
+from sagemaker.sklearn.estimator import SKLearn
+from sagemaker.workflow.steps import CacheConfig, ProcessingStep, TrainingStep
 
 from pipeliner.factory import SagemakerPipelineFactory
 
@@ -28,6 +29,11 @@ class RecommenderPipeline(SagemakerPipelineFactory):
             framework="sklearn",
             region=session.boto_region_name,
             version="1.2-1",
+        )
+
+        cache_config = CacheConfig(
+            enable_caching=True,
+            expire_after="P30d" # 30 days
         )
 
         processor = ScriptProcessor(
@@ -56,11 +62,29 @@ class RecommenderPipeline(SagemakerPipelineFactory):
             ],
         )
 
+        sklearn_estimator = SKLearn(
+            entry_point="pipelines/code/item_recommender_train.py",
+            role=role,
+            image_uri=image_uri,
+            instance_type=instance_type,
+            sagemaker_session=session,
+            base_job_name="training_job",
+            # hyperparameters=hyperparameters,
+            enable_sagemaker_metrics=True,
+        )
+
+        training_step = TrainingStep(
+            name="Train",
+            estimator=sklearn_estimator,
+            cache_config=cache_config
+        )
+
         return Pipeline(
             name=name,
             steps=[
                 user_item_matrix_step, 
-                item_similarity_matrix_step
+                item_similarity_matrix_step,
+                training_step
             ],
             sagemaker_session=session,
             parameters=[instance_type],
