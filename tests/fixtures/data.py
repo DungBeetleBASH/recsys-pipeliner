369b8002c3543986d14c40e46bfa672b28c9cc25
Pappa <dr.pappa@gmail.com>
2024-07-24 20:17:25 +0100

    SKLearn pipeline

--- tests/fixtures/data.py
+++ tests/fixtures/data.py
@@ -10,7 +10,7 @@ from pipeliner.recommendations.transformer import (
 @pytest.fixture
 def fx_user_item_ratings():
     yield pd.read_csv(
-        "tests/test_data/test_user_item_ratings.csv",
+        "tests/test_data/user_item_ratings.csv",
         dtype={"user_id": str, "item_id": str},
     )
 
